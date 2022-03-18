4651736171ee5228b73299ff8faf8a20f7f1e47a
Timur Khrustalyov <timur.khrustalyov@bstd.ru>
2022-03-18 20:47:11 +0300

    ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div>Weather App</div>
+
+    <div>
+      Weather App
+    </div>
+
   );
 }
 
