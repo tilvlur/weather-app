a2c0b6ec277120b9eac900a22b285be169d09f02
Timur Khrustalyov <timur.khrustalyov@bstd.ru>
2022-03-24 15:23:55 +0300

    404 move to new place

---
+++ src/app/NotFound404/NotFound.tsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div>404 There nothing here!</div>;
+}
+
+export default NotFound;
--- src/screens/404/index.tsx
+++
@@ -1,7 +0,0 @@
-import { memo } from "react";
-
-function NotFoundScreen() {
-  return <main>404 There nothing here!</main>;
-}
-
-export default memo(NotFoundScreen);
